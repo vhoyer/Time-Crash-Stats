00dc7578ff8f6cb6de07a8ac317c8647b3462699
vhoyer <vhoyer@live.com>
2017-04-29 11:47:17 -0300

    Added icon for the app
    Signed-off-by: vhoyer <vhoyer@live.com>

--- app/src/main/java/io/github/vhoyer/timecrashstats/MainActivity.java
+++ app/src/main/java/io/github/vhoyer/timecrashstats/MainActivity.java
@@ -80,17 +80,17 @@ public class MainActivity extends AppCompatActivity
 		// Handle navigation view item clicks here.
 		int id = item.getItemId();
 
-		if (id == R.id.nav_camera) {
+		if (id == R.id.nav_performance) {
 			// Handle the camera action
-		} else if (id == R.id.nav_gallery) {
+		} else if (id == R.id.nav_topPlay) {
 
-		} else if (id == R.id.nav_slideshow) {
+		} else if (id == R.id.nav_ranking) {
 
-		} else if (id == R.id.nav_manage) {
+		} else if (id == R.id.nav_resync) {
+
+		} else if (id == R.id.nav_savedPlays) {
 
 		} else if (id == R.id.nav_share) {
-
-		} else if (id == R.id.nav_send) {
 
 		}
 
