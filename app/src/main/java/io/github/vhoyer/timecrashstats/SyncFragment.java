ed89b4e22de7f4df361c4f547d9a8a78956b7388
vhoyer <vhoyer@live.com>
2017-05-12 14:38:11 -0300

    Support for screen rotation in resync fragment
    Signed-off-by: vhoyer <vhoyer@live.com>

--- app/src/main/java/io/github/vhoyer/timecrashstats/SyncFragment.java
+++ app/src/main/java/io/github/vhoyer/timecrashstats/SyncFragment.java
@@ -4,15 +4,19 @@ package io.github.vhoyer.timecrashstats;
 import android.app.Fragment;
 import android.content.DialogInterface;
 import android.content.Intent;
+import android.content.res.Configuration;
 import android.os.Bundle;
 import android.support.v7.app.AlertDialog;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.Button;
+import android.widget.LinearLayout;
 
 import com.google.zxing.integration.android.IntentIntegrator;
 import com.google.zxing.integration.android.IntentResult;
+
+import static android.widget.LinearLayout.HORIZONTAL;
 
 
 /**
@@ -30,6 +34,13 @@ public class SyncFragment extends Fragment implements View.OnClickListener {
 
 		Button btnSync = (Button) getActivity().findViewById(R.id.resync_btnsync);
 		btnSync.setOnClickListener(this);
+
+		LinearLayout linlayout = (LinearLayout) getActivity().findViewById(R.id.resync_linearLayout);
+		int screenOrientation = getResources().getConfiguration().orientation;
+		//if is in landscape
+		if (screenOrientation == Configuration.ORIENTATION_LANDSCAPE){
+			linlayout.setOrientation(HORIZONTAL);
+		}
 	}
 
 	@Override
@@ -43,45 +54,45 @@ public class SyncFragment extends Fragment implements View.OnClickListener {
 		scanQR();
 	}
 
-    public void scanQR(){
-        IntentIntegrator integrator = new FragmentIntentIntegrator(this);
+	public void scanQR(){
+		IntentIntegrator integrator = new FragmentIntentIntegrator(this);
 
-        integrator.setDesiredBarcodeFormats(IntentIntegrator.QR_CODE_TYPES);
-        integrator.setPrompt("Scan a QR code");
-        integrator.setCameraId(0);  // Use a specific camera of the device
-        integrator.setBeepEnabled(false);
-        integrator.setBarcodeImageEnabled(true);
-        integrator.setOrientationLocked(false);
+		integrator.setDesiredBarcodeFormats(IntentIntegrator.QR_CODE_TYPES);
+		integrator.setPrompt("Scan a QR code");
+		integrator.setCameraId(0);  // Use a specific camera of the device
+		integrator.setBeepEnabled(false);
+		integrator.setBarcodeImageEnabled(true);
+		integrator.setOrientationLocked(false);
 
-        integrator.initiateScan();
-    }
+		integrator.initiateScan();
+	}
 
-    @Override
-    public void onActivityResult(int requestCode, int resultCode, Intent data) {
-        IntentResult result = IntentIntegrator.parseActivityResult(requestCode, resultCode, data);
+	@Override
+	public void onActivityResult(int requestCode, int resultCode, Intent data) {
+		IntentResult result = IntentIntegrator.parseActivityResult(requestCode, resultCode, data);
 
-        if(result != null) {
-            if(result.getContents() == null) {
-                new AlertDialog.Builder(this.getActivity())
-                        .setTitle("not Scanned")
-                        .setMessage("There was an error")
-                        .setPositiveButton(android.R.string.ok, new DialogInterface.OnClickListener() {
-                            public void onClick(DialogInterface dialog, int which) {
-                            }
-                        })
-                        .show();
-            } else {
-                new AlertDialog.Builder(this.getActivity())
-                        .setTitle("Scanned")
-                        .setMessage(result.getContents())
-                        .setPositiveButton(android.R.string.ok, new DialogInterface.OnClickListener() {
-                            public void onClick(DialogInterface dialog, int which) {
-                            }
-                        })
-                        .show();
-            }
-        } else {
-            super.onActivityResult(requestCode, resultCode, data);
-        }
-    }
+		if(result != null) {
+			if(result.getContents() == null) {
+				new AlertDialog.Builder(this.getActivity())
+					.setTitle("not Scanned")
+					.setMessage("There was an error")
+					.setPositiveButton(android.R.string.ok, new DialogInterface.OnClickListener() {
+						public void onClick(DialogInterface dialog, int which) {
+						}
+					})
+				.show();
+			} else {
+				new AlertDialog.Builder(this.getActivity())
+					.setTitle("Scanned")
+					.setMessage(result.getContents())
+					.setPositiveButton(android.R.string.ok, new DialogInterface.OnClickListener() {
+						public void onClick(DialogInterface dialog, int which) {
+						}
+					})
+				.show();
+			}
+		} else {
+			super.onActivityResult(requestCode, resultCode, data);
+		}
+	}
 }
